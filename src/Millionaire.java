a1a56de92c6b7ece6ec184c270f16d432ca20236
Harish <harishkumar@industrylab.io>
2018-04-27 14:20:30 +0530

    second

---
+++ src/Millionaire.java
@@ -0,0 +1,15 @@
+
+public class Millionaire {
+
+	public static void main(String[] args) {
+		 // TODO Auto-generated method stub
+	     double amountInAccount;
+		amountInAccount = 50.22;
+		amountInAccount = amountInAccount + 1_000_000.00;
+		System.out.print("You have $");
+		System.out.print(amountInAccount);
+		System.out.println(" in your account.");
+
+	}
+
+}
